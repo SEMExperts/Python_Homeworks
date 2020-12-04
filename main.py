1174a3dfc265a55ad245fcaac2c60a9716114e7d
Artem Zorin <marketing@semexperts.com.ua>
2020-12-05 00:21:25 +0200

    Create parse_parameters func

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-def parse_parameters(query: str) -> dict:
-    return {}
+def parse_parameters(str):
+    lst = str[str.find('?') + 1:].split('&')
+    key = []
+    value = []
+    for i in range(len(lst)):
+        key.append(lst[i].split('=')[0])
+        value.append(lst[i].split('=')[1])
+
+    return dict(zip(key, value))
 
 
-def parse_cookies(query: str) -> dict:
-    return {}
+# def parse_cookies(query: str) -> dict:
+#     return {}
 
 
 if __name__ == '__main__':
     # Tests for function "parse_parameters"
-    assert parse_parameters('http://example.com/?') == {}
-    assert parse_parameters('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
+    print(parse_parameters('https://example.com/path/to/page?name=ferret&color=purple'))
+    print(parse_parameters('http://example.com?product=1234&utm_source=google'))
+    print(parse_parameters('https://example.com/products/women?category=dresses&color=green'))
+    print(parse_parameters('https://example.com/products/women/dresses/green.html?limit=20&sessionid=123'))
+    print(parse_parameters('example.com?type=dress&color=green'))
+    print(parse_parameters('http://www.examples.com/search?category=shoe&brand=nike&color=red&size=5&pricefrom=10&priceto=1000.'))
+    print(parse_parameters('http://www.example.com?shopping-category=shoes&sort-by=size&sort-order=asc'))
+    print(parse_parameters('http://www.example.com?shopping-category=DVD-movies&sort-by=production-year&sort-order=asc'))
+    print(parse_parameters('http://www.example.com?country=fr'))
+    print(parse_parameters('https://www.onlinestor.com?product=socks&color=black&size=m'))
 
-    # Tests for function "parse_cookies"
-    assert parse_cookies('') == {}
-    assert parse_cookies('name=Dima;') == {'name': 'Dima'}
+
+
+    # # Tests for function "parse_cookies"
+    # assert parse_cookies('') == {}
+    # assert parse_cookies('name=Dima;') == {'name': 'Dima'}
