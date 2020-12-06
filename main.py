de4c3f8edc5f3d9209f5c827f6cd45345f4d6eb2
Artem Zorin <marketing@semexperts.com.ua>
2020-12-07 00:13:10 +0200

    Added assert testing

--- main.py
+++ main.py
@@ -29,27 +29,28 @@ def parse_cookies(str):
 
 if __name__ == '__main__':
     # Tests for function "parse_parameters"
-    print(parse_parameters('https://example.com/'))
-    print(parse_parameters('http://example.com?product=1234&utm_source=google'))
-    print(parse_parameters('https://example.com/products/women?category=dresses&color=green'))
-    print(parse_parameters('https://example.com/products/women/dresses/green.html?limit=20&sessionid=123'))
-    print(parse_parameters('example.com?type=dress&color=green'))
-    print(parse_parameters('http://www.examples.com/search?category=shoe&brand=nike&color=red&size=5&pricefrom=10&priceto=1000.'))
-    print(parse_parameters('http://www.example.com?shopping-category=shoes&sort-by=size&sort-order=asc'))
-    print(parse_parameters('http://www.example.com?shopping-category=DVD-movies&sort-by=production-year&sort-order=asc'))
-    print(parse_parameters('http://www.example.com?country=fr'))
-    print(parse_parameters('https://www.onlinestor.com?product=socks&color=black&size=m'))
+    assert parse_parameters('http://example.com/?') == {}
+    assert parse_parameters('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
+    assert parse_parameters('http://example.com?product=1234&utm_source=google') == {'product': '1234', 'utm_source': 'google'}
+    assert parse_parameters('https://example.com/products/women?category=dresses&color=green') == {'category': 'dresses', 'color': 'green'}
+    assert parse_parameters('https://example.com/products/women/dresses/green.html?limit=20&sessionid=123') == {'limit': '20', 'sessionid': '123'}
+    assert parse_parameters('example.com?type=dress&color=green') == {'type': 'dress', 'color': 'green'}
+    assert parse_parameters('http://www.examples.com/search?category=shoe&brand=nike&color=red&size=5&pricefrom=10&priceto=1000.') == {'category': 'shoe', 'brand': 'nike', 'color': 'red', 'size': '5', 'pricefrom': '10', 'priceto': '1000.'}
+    assert parse_parameters('http://www.example.com?shopping-category=shoes&sort-by=size&sort-order=asc') == {'shopping-category': 'shoes', 'sort-by': 'size', 'sort-order': 'asc'}
+    assert parse_parameters('http://www.example.com?shopping-category=DVD-movies&sort-by=production-year&sort-order=asc') == {'shopping-category': 'DVD-movies', 'sort-by': 'production-year', 'sort-order': 'asc'}
+    assert parse_parameters('http://www.example.com?country=fr') == {'country': 'fr'}
+    assert parse_parameters('https://www.onlinestor.com?product=socks&color=black&size=m') == {'product': 'socks', 'color': 'black', 'size': 'm'}
 
     # Tests for function "parse_cookies"
-    print(parse_cookies(''))
-    print(parse_cookies('name=Dima;'))
-    print(parse_cookies('NAME=VALUE;expires=DATE;path=PATH;domain=DOMAIN_NAME;'))
-    print(parse_cookies('NAME=VALUE;'))
-    print(parse_cookies('NAME1=OPAQUE_STRING1; NAME2=OPAQUE_STRING2'))
-    print(parse_cookies('CUSTOMER=WILE_E_COYOTE; path=/; expires=Wednesday,'))
-    print(parse_cookies('PART_NUMBER=ROCKET_LAUNCHER_0001;'))
-    print(parse_cookies('CUSTOMER=WILE_E_COYOTE; PART_NUMBER=ROCKET_LAUNCHER_0001; SHIPPING=FEDEX'))
-    print(parse_cookies('PART_NUMBER=RIDING_ROCKET_0023;'))
-    print(parse_cookies('PART_NUMBER=RIDING_ROCKET_0023; PART_NUMBER=ROCKET_LAUNCHER_0001'))
+    assert parse_cookies('') == {}
+    assert parse_cookies('name=Dima;') == {'name': 'Dima'}
+    assert parse_cookies('NAME=VALUE;expires=DATE;path=PATH;domain=DOMAIN_NAME;') == {'NAME': 'VALUE', 'expires': 'DATE', 'path': 'PATH', 'domain': 'DOMAIN_NAME'}
+    assert parse_cookies('NAME=VALUE;') == {'NAME': 'VALUE'}
+    assert parse_cookies('NAME1=OPAQUE_STRING1; NAME2=OPAQUE_STRING2') == {'NAME1': 'OPAQUE_STRING1', 'NAME2': 'OPAQUE_STRING2'}
+    assert parse_cookies('CUSTOMER=WILE_E_COYOTE; path=/; expires=Wednesday,') == {'CUSTOMER': 'WILE_E_COYOTE', 'path': '/', 'expires': 'Wednesday,'}
+    assert parse_cookies('PART_NUMBER=ROCKET_LAUNCHER_0001;') == {'PART_NUMBER': 'ROCKET_LAUNCHER_0001'}
+    assert parse_cookies('CUSTOMER=WILE_E_COYOTE; PART_NUMBER=ROCKET_LAUNCHER_0001; SHIPPING=FEDEX') == {'CUSTOMER': 'WILE_E_COYOTE', 'PART_NUMBER': 'ROCKET_LAUNCHER_0001', 'SHIPPING': 'FEDEX'}
+    assert parse_cookies('PART_NUMBER=RIDING_ROCKET_0023;') == {'PART_NUMBER': 'RIDING_ROCKET_0023'}
+    assert parse_cookies('PART_NUMBER=RIDING_ROCKET_0023; PART_NUMBER=ROCKET_LAUNCHER_0001') == {'PART_NUMBER': 'ROCKET_LAUNCHER_0001'}
 
 
